73208a7f0c79ce83536bed7b3c708f47255b2847
ALi Beydes <tureks@Turekss-MacBook-Pro.local>
2026-04-21 15:27:09 +0300

    Remove hardcoded NEXT_PUBLIC_API_URL from next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: 'dist',
-  env: {
-    NEXT_PUBLIC_API_URL: 'https://task.agudo.net/api',
-  },
   async rewrites() {
     return [
       {
